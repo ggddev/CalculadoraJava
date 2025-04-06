beda2f684bdaf45e86a761fd3322788ed304636c
Guilherme Gonçalves Dantas <guilhermedantas162@gmail.com>
2025-04-06 12:59:25 -0300

    feat classes
    Foi construída a estrutura da calculadora e adicionamos uma classe para realizar os cálculos. Foi criado o objeto que leva os dados para a classe de cálculos.

---
+++ CalculadoraggdDev.java
@@ -0,0 +1,27 @@
+package CalculadoraJava;
+import java.util.Scanner;
+public class CalculadoraggdDev {
+
+    public static void main(String[] args) {
+        Scanner at = new Scanner(System.in);
+        
+        System.out.println("Olá! Bem-vindo a ggdDevCalculadora!\n");
+        System.out.println("Nos diga seu nome: ");
+        String nomeUser = at.nextLine();
+        
+        System.out.println("Informe o primeiro número do seu cálculo: ");
+        double num1 = at.nextDouble();
+        at.nextLine();
+        System.out.println("Informe o segundo número do seu cálculo: ");
+        double num2 = at.nextDouble();
+        at.nextLine();
+        
+        System.out.println("Agora informe o tipo de operador: (1) Adição | (2) Subtração | (3) Multiplicação | (4) Divisão");
+        int respOperador = at.nextInt();
+        at.nextLine();
+        
+        Calculos conta = new Calculos(num1, num2, respOperador);
+        
+    }
+    
+}
---
+++ Calculos.java
@@ -0,0 +1,36 @@
+package CalculadoraJava;
+public class Calculos {
+    private double num1;
+    private double num2;
+    private int operador;
+    
+    public Calculos(double num1, double num2, int operador){
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operador = operador;
+    }
+    
+    public double getNum1(){
+        return num1;
+    }
+    
+    public void setNum1(double num1){
+        this.num1 = num1;
+    }
+    
+    public double getNum2(){
+        return num2;
+    }
+    
+    public void setNum2(double num2){
+        this.num2 = num2;
+    }
+    
+    public int getOperador(){
+        return operador;
+    }
+    
+    public void setOperador(int operador){
+        this.operador = operador;
+    }
+}
